3d0e4ad8fe13b2a0a7ebe5ae252dc829d7eeadab
poteboy <poteboy.dev@gmail.com>
2023-04-04 23:55:20 +0900

    Simplify space function and fix TypeScript error

--- src/system/space.ts
+++ src/system/space.ts
@@ -1,18 +1,49 @@
 import { toCssUnit } from "./util";
-export interface SpaceProps {
-  m?: string | number;
-  mt?: string | number;
-}
+type SpaceKeys =
+  | "m"
+  | "mt"
+  | "mr"
+  | "mb"
+  | "ml"
+  | "mx"
+  | "my"
+  | "p"
+  | "pt"
+  | "pr"
+  | "pb"
+  | "pl"
+  | "px"
+  | "py";
+
+export type SpaceProps = Partial<Record<SpaceKeys, string | number>>;
+
+const spaceMappings: Record<SpaceKeys, string> = {
+  m: "margin",
+  mt: "margin-top",
+  mr: "margin-right",
+  mb: "margin-bottom",
+  ml: "margin-left",
+  mx: "margin-left,margin-right",
+  my: "margin-top,margin-bottom",
+  p: "padding",
+  pt: "padding-top",
+  pr: "padding-right",
+  pb: "padding-bottom",
+  pl: "padding-left",
+  px: "padding-left,padding-right",
+  py: "padding-top,padding-bottom",
+};
 
 export const space = (props: SpaceProps): string => {
   let styles = "";
-
-  if (props.m) {
-    styles += `margin: ${toCssUnit(props.m)}; `;
-  }
-
-  if (props.mt) {
-    styles += `margin-top: ${toCssUnit(props.mt)}; `;
+  for (const key in spaceMappings) {
+    const cssValue = props[key as SpaceKeys];
+    if (cssValue) {
+      const properties = spaceMappings[key as SpaceKeys].split(",");
+      for (const property of properties) {
+        styles += `${property}: ${toCssUnit(cssValue)}; `;
+      }
+    }
   }
 
   return styles;
