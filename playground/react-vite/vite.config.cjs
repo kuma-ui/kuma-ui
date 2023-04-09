e458108459ff5b063682665b957f165fa2f1c1cd
poteboy <poteboy.dev@gmail.com>
2023-04-09 16:41:05 +0900

    update vite config to use compatible module formats

--- playground/react-vite/vite.config.cjs
+++ playground/react-vite/vite.config.cjs
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 import zeroStyled from "zero-styled/dist/vite/index.js";
 import { resolve } from "path";
 
+// - Rename vite.config.js to vite.config.cjs to ensure the use of CommonJS module format.
+// - Update import statement to import the .js file instead of .mjs, ensuring the correct module format is used in Vite configuration.
+// - This change ensures consistent module formats and better compatibility with the Node.js environment used by Vite.
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
