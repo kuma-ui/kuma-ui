4e317bb881675c68098a60cdf52ffacdfcc41a66
poteboy <poteboy.dev@gmail.com>
2023-04-10 14:53:07 +0900

    add test for sheet class

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+import { Config } from "@jest/types";
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  testMatch: ["<rootDir>/**/*.test.ts"],
+  testEnvironment: "node",
+  collectCoverage: true,
+  errorOnDeprecated: true,
+};
+export default config;
---
+++ src/sheet/sheet.test.ts
@@ -0,0 +1,38 @@
+import { Sheet } from ".";
+import { describe, expect, test, beforeEach } from "@jest/globals";
+
+describe("Sheet class", () => {
+  let sheet: Sheet;
+  beforeEach(() => {
+    sheet = new Sheet();
+  });
+
+  test("addRule() should add a new rule with a generated ID", () => {
+    // Arrange
+    const css = "color: red;";
+    // Act
+    const id = sheet.addRule(css);
+    // Assert
+    expect(id.startsWith("zero")).toBeTruthy();
+  });
+
+  test("addRule() should not add duplicate rules", () => {
+    // Arrange
+    const css = "color: red;";
+    // Act
+    const id1 = sheet.addRule(css);
+    const id2 = sheet.addRule(css);
+    // Assert
+    expect(id1).toBe(id2);
+  });
+
+  test("getCSS() should return the CSS string with unique rules", () => {
+    // Arrange
+    const css = "color: red;";
+    const id = sheet.addRule(css);
+    // Act
+    const cssString = sheet.getCSS();
+    // Assert
+    expect(cssString).toContain(`.${id} {${css}}`);
+  });
+});
