1e7cac41a56bea12241507dafe50a2228266adf6
Karibash <y.karibayashi@gmail.com>
2023-05-05 14:22:08 +0900

    Remove unnecessary plugins
    https://tsup.egoist.dev/#excluding-packages

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'tsup';
-import external from "rollup-plugin-peer-deps-external";
 
 export default defineConfig({
   clean: true,
   format: ['cjs', 'esm'],
-  plugins: [external()],
 });
