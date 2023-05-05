55a0bbebfcd0a6db63a05eef09611e36f7faa403
Karibash <y.karibayashi@gmail.com>
2023-05-05 14:19:40 +0900

    Make the dist directory be deleted before build

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'tsup';
 import external from "rollup-plugin-peer-deps-external";
 
 export default defineConfig({
+  clean: true,
   format: ['cjs', 'esm'],
   plugins: [external()],
 });
