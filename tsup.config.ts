304ad086d52b230fcb93767abffa12810e849b25
Karibash <y.karibayashi@gmail.com>
2023-05-05 14:17:51 +0900

    Make it possible to specify the format at build time in the config file

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'tsup';
 import external from "rollup-plugin-peer-deps-external";
 
 export default defineConfig({
+  format: ['cjs', 'esm'],
   plugins: [external()],
 });
