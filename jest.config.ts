ac12460f620ff7c4bcb29d5d383e1ad83cfc6beb
Karibash <y.karibayashi@gmail.com>
2023-05-06 13:21:57 +0900

    Make the tests run on a package-by-package basis

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-import { Config } from "@jest/types";
-const config: Config.InitialOptions = {
-  preset: "ts-jest",
-  testMatch: ["<rootDir>/**/*.test.ts"],
-  testEnvironment: "node",
-  collectCoverage: true,
-  errorOnDeprecated: true,
-};
-export default config;
---
+++ packages/system/jest.config.ts
@@ -0,0 +1,7 @@
+import { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  preset: "@kuma-ui/jest-preset",
+};
+
+export default config;
