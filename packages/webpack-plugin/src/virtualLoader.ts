fdf000795a204a8921e11ba69ef2d13baacf93af
poteboy <poteboy.dev@gmail.com>
2023-05-11 14:26:39 +0900

    remove unnecessary typescript devDependencies

---
+++ packages/webpack-plugin/src/virtualLoader.ts
@@ -0,0 +1,8 @@
+import type { LoaderContext } from "webpack";
+
+function virtualLoader(this: LoaderContext<{ src: string }>): string {
+  const { src } = this.getOptions();
+  return src;
+}
+
+export default virtualLoader;
