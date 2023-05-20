b0307828fe3440368f5f21226f9d259903ef6cb0
poteboy <poteboy.dev@gmail.com>
2023-05-20 21:52:23 +0900

    process style object via AST

--- packages/core/src/css.ts
+++ packages/core/src/css.ts
@@ -1,8 +1,6 @@
 import { StyledProps, PseudoProps } from "@kuma-ui/system";
 
-type CSSFunction = {
-  (styles: StyledProps & PseudoProps): string;
-};
+type CSSFunction = (styles: StyledProps & PseudoProps) => string;
 
 export const css: CSSFunction = () => {
   throw Error('Using the "css" in runtime is not supported.');
