5f3ec575942642b273dc476f935ad0290d4b1dbe
poteboy <poteboy.dev@gmail.com>
2023-05-22 10:33:56 +0900

    Refactor tests for css function to follow AAA pattern

--- packages/babel-plugin/src/__test__/css.test.ts
+++ packages/babel-plugin/src/__test__/css.test.ts
@@ -3,43 +3,63 @@ import { babelTransform } from "./testUtils";
 
 describe("css function", () => {
   test("should contain CSS prefix unique to Kuma UI", () => {
-    const { code } = babelTransform(`
+    // Arrange
+    const inputCode = `
         import { css } from '@kuma-ui/core'
         const style = css({ color: 'red' })
-    `);
+    `;
+    // Act
+    const { code } = babelTransform(inputCode);
+    // Assert
     expect(code).toContain("kuma-");
   });
 
   test("should work correctly when css function is renamed", () => {
-    const { code } = babelTransform(`
+    // Arrange
+    const inputCode = `
         import { css as c } from '@kuma-ui/core'
         const style = c({ color: 'red' })
-    `);
+    `;
+    // Act
+    const { code } = babelTransform(inputCode);
+    // Assert
     expect(code).toContain("kuma-");
   });
 
   describe("Snapshot tests", () => {
     test("basic usage should match snapshot", () => {
-      const { code } = babelTransform(`
+      // Arrange
+      const inputCode = `
         import { css } from '@kuma-ui/core'
         const style = css({ color: 'red' })
-      `);
+      `;
+      // Act
+      const { code } = babelTransform(inputCode);
+      // Assert
       expect(code).toMatchSnapshot();
     });
 
     test("using space props should match snapshot", () => {
-      const { code } = babelTransform(`
+      // Arrange
+      const inputCode = `
         import { css } from '@kuma-ui/core'
         const style = css({ p: 2 })
-      `);
+      `;
+      // Act
+      const { code } = babelTransform(inputCode);
+      // Assert
       expect(code).toMatchSnapshot();
     });
 
     test("using pseudo props should match snapshot", () => {
-      const { code } = babelTransform(`
+      // Arrange
+      const inputCode = `
         import { css } from '@kuma-ui/core'
         const style = css({ _hover: { color: 'red' } })
-      `);
+      `;
+      // Act
+      const { code } = babelTransform(inputCode);
+      // Assert
       expect(code).toMatchSnapshot();
     });
   });
