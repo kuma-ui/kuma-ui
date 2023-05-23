7a34060f0c64c858ec0047ae8365cdb10218b8a1
taishinaritomi <taishinaritomi@gmail.com>
2023-05-23 21:25:25 +0900

    remove unnecessary babel tests

--- packages/babel-plugin/src/__test__/css.test.ts
+++ packages/babel-plugin/src/__test__/css.test.ts
@@ -2,30 +2,6 @@ import { describe, expect, test } from "@jest/globals";
 import { babelTransform } from "./testUtils";
 
 describe("css function", () => {
-  test("should contain CSS prefix unique to Kuma UI", () => {
-    // Arrange
-    const inputCode = `
-        import { css } from '@kuma-ui/core'
-        const style = css({ color: 'red' })
-    `;
-    // Act
-    const { code } = babelTransform(inputCode);
-    // Assert
-    expect(code).toContain("kuma-");
-  });
-
-  test("should work correctly when css function is renamed", () => {
-    // Arrange
-    const inputCode = `
-        import { css as c } from '@kuma-ui/core'
-        const style = c({ color: 'red' })
-    `;
-    // Act
-    const { code } = babelTransform(inputCode);
-    // Assert
-    expect(code).toContain("kuma-");
-  });
-
   describe("Snapshot tests", () => {
     test("basic usage should match snapshot", () => {
       // Arrange
