5bab97668df6cac4503dd57bea672bec641ff627
poteboy <poteboy.dev@gmail.com>
2023-05-28 13:21:56 +0900

    effect

---
+++ packages/system/src/effect.ts
@@ -0,0 +1,44 @@
+import { EffectKeys } from "./keys";
+import { CSSProperties, CSSValue, ResponsiveStyle } from "./types";
+import { applyResponsiveStyles } from "./responsive";
+
+export type EffectProps = Partial<
+  CSSProperties<
+    | "transition"
+    | "transitionDuration"
+    | "transitionProperty"
+    | "transitionTimingFunction"
+    | "transitionDelay"
+    | "transform"
+    | "transformOrigin"
+  >
+>;
+
+const effectMappings: Record<EffectKeys, string> = {
+  transition: "transition",
+  transitionDuration: "transition-duration",
+  transitionProperty: "transition-property",
+  transitionTimingFunction: "transition-timing-function",
+  transitionDelay: "transition-delay",
+  transform: "transform",
+  transformOrigin: "transform-origin",
+} as const;
+
+export const effect = (props: EffectProps): ResponsiveStyle => {
+  let base = "";
+  const media: ResponsiveStyle["media"] = {};
+
+  for (const key in effectMappings) {
+    const cssValue = props[key as EffectKeys];
+    if (cssValue) {
+      const property = effectMappings[key as EffectKeys];
+      const responsiveStyles = applyResponsiveStyles(property, cssValue);
+      base += responsiveStyles.base;
+      for (const [breakpoint, css] of Object.entries(responsiveStyles.media)) {
+        if (media[breakpoint]) media[breakpoint] += css;
+        else media[breakpoint] = css;
+      }
+    }
+  }
+  return { base, media };
+};
