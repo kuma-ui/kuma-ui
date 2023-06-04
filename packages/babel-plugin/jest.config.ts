6e568bce88f7190aef0c25aa6366ab59c98dcc3c
Yo Iwamoto <you.iwamoto918@gmail.com>
2023-06-04 21:48:25 +0900

    migrate babel-plugin test to vitest

--- packages/babel-plugin/jest.config.ts
+++
@@ -1,7 +0,0 @@
-import { Config } from "@jest/types";
-
-const config: Config.InitialOptions = {
-  preset: "@kuma-ui/jest-preset",
-};
-
-export default config;
---
+++ packages/babel-plugin/vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "node",
+    coverage: {
+      enabled: true,
+      reporter: ["clover", "json", "lcov"],
+    },
+  },
+});
