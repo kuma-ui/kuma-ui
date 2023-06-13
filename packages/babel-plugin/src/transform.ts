67c2602a83f1161c1032f363cfe085286ebe8583
kotarella1110 <kotarella1110@gmail.com>
2023-06-14 01:45:22 +0900

    feat(babel-plugin): move kuma-ui transform from transformFromAstSync to transformSync

--- packages/babel-plugin/src/transform.ts
+++ packages/babel-plugin/src/transform.ts
@@ -1,11 +1,7 @@
-import { types as t, parseSync, transformFromAstSync } from "@babel/core";
-import type { ParseResult } from "@babel/core";
+import { transformSync } from "@babel/core";
 
 export async function transform(code: string, id: string) {
-  const file: ParseResult | null = parseSync(code);
-  if (!file) return;
-
-  const result = await transformFromAstSync(file, code, {
+  const result = await transformSync(code, {
     filename: id,
     sourceMaps: true,
     plugins: [require("./index").default],
