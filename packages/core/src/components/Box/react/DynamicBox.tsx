9231dc88aa4d76a32543b8d7d2a4a31ec93ca5c9
kotarella1110 <kotarella1110@gmail.com>
2023-06-25 11:12:29 +0900

    feat(core): support client to DynamicBox

--- packages/core/src/components/Box/react/DynamicBox.tsx
+++ packages/core/src/components/Box/react/DynamicBox.tsx
@@ -2,11 +2,34 @@
 
 import React from "react";
 import type { BoxComponent } from "./types";
+import { isBrowser } from "./isBrowser";
+import { useStyleRegistry, createStyleRegistry } from "./StyleRegistry";
 
+const defaultRegistry = isBrowser ? createStyleRegistry() : null;
+
+const useInsertionEffect = React.useInsertionEffect || React.useLayoutEffect;
+
+// TODO: support SSR
 export const DynamicBox: BoxComponent = ({
   as: Component = "div",
   children,
   ...props
 }) => {
-  return React.createElement(Component, props, children);
+  const box = React.createElement(Component, props, children);
+  const registry = defaultRegistry ? defaultRegistry : useStyleRegistry();
+
+  useInsertionEffect(() => {
+    if (registry === null) {
+      return;
+    }
+    // TODO: generate id and rule
+    const id = "test";
+    const rule = "div { color: red }";
+    registry.add(id, rule);
+    return () => {
+      registry.remove(id);
+    };
+  }, []);
+
+  return box;
 };
