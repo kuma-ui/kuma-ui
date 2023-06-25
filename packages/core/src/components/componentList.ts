f365b6f09a28fb58f7b4bbd72be960eba88d1fbc
poteboy <poteboy.dev@gmail.com>
2023-06-25 16:13:59 +0900

    Revert "componentHandler"
    This reverts commit 5ea9223591b8a8f087f83303aac3e3ac24b5c841.

--- packages/core/src/components/componentList.ts
+++ packages/core/src/components/componentList.ts
@@ -1,5 +1,4 @@
-import { Box, isBoxProps, Flex, Spacer, Text, Button, Heading } from "./index";
-import { match } from "ts-pattern";
+import { Box, Flex, Spacer, Text, Button, Heading } from "./index";
 
 export const componentList = Object.freeze({
   Box: Box.name as "Box",
@@ -9,16 +8,3 @@ export const componentList = Object.freeze({
   Button: Button.name as "Button",
   Heading: Heading.name as "Heading",
 } as const);
-
-type ComponentName = keyof typeof componentList;
-
-export const isComponentProps =
-  (componentName: ComponentName) => (propName: string) => {
-    return match(componentName)
-      .with("Box", isBoxProps)
-      .otherwise(() => false);
-    // .exhaustive();
-  };
-
-export const componentHandler =
-  (componentName: ComponentName) => (props: Record<string, any>) => {};
