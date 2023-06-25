495b14545e5b1a97f3645a669380b1087b16cd90
kotarella1110 <kotarella1110@gmail.com>
2023-06-25 11:11:17 +0900

    feat(core): add StyleSheetRegistry

---
+++ packages/core/src/components/Box/react/StyleSheetRegistry.ts
@@ -0,0 +1,37 @@
+import { isProduction } from "./isProduction";
+import { StyleSheet } from "./StyleSheet";
+
+export class StyleSheetRegistry {
+  private sheet: StyleSheet;
+  private indices: Record<string, number> = {};
+
+  constructor({
+    styleSheet = null,
+    speedy = isProduction,
+  }: { styleSheet?: StyleSheet | null; speedy?: boolean } = {}) {
+    this.sheet = styleSheet || new StyleSheet("kuma-ui", speedy);
+
+    this.sheet.inject();
+    if (styleSheet) {
+      this.sheet.setSpeedy(speedy);
+    }
+  }
+
+  public add(id: string, rule: string): void {
+    const index = this.sheet.insertRule(rule);
+    if (index === -1) {
+      return;
+    }
+
+    this.indices[id] = index;
+  }
+
+  public remove(id: string): void {
+    if (id in this.indices) {
+      throw new Error(`StyleSheetRegistry: styleId: \`${styleId}\` not found.`);
+    }
+
+    this.sheet.deleteRule(this.indices[id]);
+    delete this.indices[id];
+  }
+}
