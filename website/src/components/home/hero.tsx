5d37b9d3d6f3544c63bccc552944ff79f3cec818
poteboy <poteboy.dev@gmail.com>
2023-07-01 15:15:34 +0900

    wip

--- website/src/components/home/hero.tsx
+++ website/src/components/home/hero.tsx
@@ -1,6 +1,21 @@
 import React from "react";
-import { Box } from "@kuma-ui/core";
+import { Box, Heading, css } from "@kuma-ui/core";
 
 export const Hero = () => {
-  return <Box>hello</Box>;
+  return (
+    <Box m={["80px auto 0px", "0px auto"]} maxWidth="1200px" px={[18, 36]}>
+      <Box position="relative">
+        <Heading
+          fontSize="2.25rem"
+          fontWeight={800}
+          className={css`
+            letter-spacing: -0.025em;
+            line-height: 2.5rem;
+          `}
+        >
+          Power Your Web Development with Optimal Performance and Flexibility
+        </Heading>
+      </Box>
+    </Box>
+  );
 };
