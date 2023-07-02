3bef11bf8fa14a57db703a39bce709a9f2fc43f2
poteboy <poteboy.dev@gmail.com>
2023-07-02 20:45:51 +0900

    gridData

--- website/src/components/home/hero.tsx
+++ website/src/components/home/hero.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export const Hero = () => {
   return (
-    <Box m={["40px auto", "80px auto 0px"]} maxWidth="1200px">
+    <Box m={["40px auto", "80px auto 0px"]}>
       <Box position="relative">
         <Box role="presentation" display={["initial"]} bg="white">
           <Box
@@ -68,6 +68,91 @@ export const Hero = () => {
           </Link>
         </Box>
       </Box>
+      <Box as="section" mt="5rem">
+        <Box
+          display="grid"
+          gap="1.5rem"
+          gridTemplateColumns={[
+            "repeat(1,minmax(0,1fr))",
+            "repeat(1,minmax(0,1fr))",
+            "repeat(3,minmax(0,1fr))",
+          ]}
+        >
+          {gridData.map((data) => (
+            <Box key={data.emoji}>
+              <Box
+                width="3rem"
+                height="3rem"
+                fontSize="24px"
+                display="flex"
+                alignItems="center"
+                justify="center"
+                bg="#f6f6f7"
+                borderRadius="6px"
+                mb="0.75rem"
+              >
+                {data.emoji}
+              </Box>
+              <Heading
+                as="h2"
+                className={css`
+                  font-size: 1.25rem;
+                  line-height: 1.75rem;
+                  font-weight: 700;
+                  margin-bottom: 0.25rem;
+                `}
+              >
+                {data.title}
+              </Heading>
+              <Text
+                className={css`
+                  font-size: 1rem;
+                  line-height: 1.5rem;
+                `}
+              >
+                {data.description}
+              </Text>
+            </Box>
+          ))}
+        </Box>
+      </Box>
     </Box>
   );
 };
+
+const gridData = [
+  {
+    emoji: "⚡️",
+    title: "Zero-runtime",
+    description:
+      "Deliver blazing-fast websites with zero-runtime CSS extraction.",
+  },
+  {
+    emoji: "🔮",
+    title: "Autocomplete",
+    description: "Experience seamless development with style autocompletion.",
+  },
+  {
+    emoji: "🛠️",
+    title: "Headless Components",
+    description:
+      "Enjoy maximum customization freedom with our headless components.",
+  },
+  {
+    emoji: "🤝",
+    title: "Hybrid Approach",
+    description:
+      "The best of both worlds—support for any writing style with our hybrid approach.",
+  },
+  {
+    emoji: "🚀",
+    title: "RSC Support",
+    description:
+      "Stay updated with cutting-edge Next.js technology via our RSC support.",
+  },
+  {
+    emoji: "🥇",
+    title: "Familiar DX",
+    description: "Delight in the ultimate DX with our familiar API design.",
+  },
+];
