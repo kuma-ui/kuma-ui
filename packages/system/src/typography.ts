1fc134713ee47e7cd6b896440de0244c5fa38ce5
masa5555 <masa57887@gmail.com>
2023-07-06 21:47:11 +0900

    add: more typography CSS to system

--- packages/system/src/typography.ts
+++ packages/system/src/typography.ts
@@ -4,13 +4,37 @@ import { ResponsiveStyle, CSSProperties } from "./types";
 import { applyResponsiveStyles } from "./responsive";
 
 export type TypographyProps = Partial<
-  CSSProperties<"lineHeight", true> & CSSProperties<"letterSpacing", true>
+  CSSProperties<"hyphenateCharacter" | "hyphens"> &
+    CSSProperties<"hyphenateLimitChars", true> &
+    CSSProperties<"hangingPunctuation"> &
+    CSSProperties<"lineHeight", true> &
+    CSSProperties<"lineBreak"> &
+    CSSProperties<"orphans", true> &
+    CSSProperties<"rubyPosition"> &
+    CSSProperties<"unicodeBidi"> &
+    CSSProperties<"widows", true> &
+    CSSProperties<"whiteSpace"> &
+    CSSProperties<"wordSpacing" | "letterSpacing", true> &
+    CSSProperties<"wordBreak" | "writingMode">
 >;
 
 const typographyMappings: Record<TypographyKeys, string> = {
+  hyphenateCharacter: "hyphenate-character",
+  hyphenateLimitChars: "hyphenate-limit-chars",
+  hyphens: "hyphens",
+  hangingPunctuation: "hanging-punctuation",
   lineHeight: "line-height",
+  lineBreak: "line-break",
+  orphans: "orphans",
+  rubyPosition: "ruby-position",
+  unicodeBidi: "unicode-bidi",
+  widows: "widows",
+  whiteSpace: "white-space",
   letterSpacing: "letter-spacing",
-};
+  wordBreak: "word-break",
+  wordSpacing: "word-spacing",
+  writingMode: "writing-mode",
+} as const;
 
 export const typography = (props: TypographyProps): ResponsiveStyle => {
   let baseStyles = "";
@@ -21,8 +45,8 @@ export const typography = (props: TypographyProps): ResponsiveStyle => {
     if (cssValue) {
       const property = typographyMappings[key as TypographyKeys];
       const converter = [
-        typographyMappings.lineHeight,
         typographyMappings.letterSpacing,
+        typographyMappings.wordSpacing,
       ].includes(property)
         ? toCssUnit
         : undefined;
