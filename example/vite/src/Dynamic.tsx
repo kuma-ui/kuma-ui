3e9deb0268ba978f3a28be18f45027ec3cf6808f
kotarella1110 <kotarella1110@gmail.com>
2023-07-09 03:00:35 +0900

    fix(vite): fix injection user theme bug

--- example/vite/src/Dynamic.tsx
+++ example/vite/src/Dynamic.tsx
@@ -5,7 +5,7 @@ export function Dynamic() {
   const [checked, toggle] = React.useReducer((state) => !state, false);
 
   return (
-    <Box p={12} color={checked ? "blue" : "green"}>
+    <Box p={12} color={checked ? "colors.blue.light" : "green"}>
       dynamic
       <Button onClick={() => toggle()}>Change Color</Button>
     </Box>
