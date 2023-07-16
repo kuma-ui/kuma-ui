237a08a9583cd69bb40ea57753cd8bdf38bc4ca9
Kengo Asamizu <asamizu.kengo@starbridge-iwate.com>
2023-07-16 15:50:21 +0900

    fix: resolution failing .mjs file of the @kuma-ui/core on Windows.

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,9 +1,55 @@
 import { defineConfig } from "tsup";
+import fs from "fs";
+import path from "path";
 
-export default defineConfig({
-  clean: true,
-  dts: true,
-  format: ["cjs", "esm"],
-  entry: ["src", "!src/**/*.test.*", "!src/**__test__/**"],
-  bundle: false,
-});
+export default defineConfig([
+  {
+    clean: true,
+    dts: true,
+    format: ["cjs"],
+    entry: ["src", "!src/**/*.test.*", "!src/**__test__/**"],
+    bundle: false,
+  },
+  {
+    clean: true,
+    format: ["esm"],
+    entry: ["src", "!src/**/*.test.*", "!src/**__test__/**"],
+    bundle: true,
+    esbuildPlugins: [
+      {
+        name: "add-extension",
+        setup(build) {
+          build.onResolve({ filter: /.*/ }, (args) => {
+            const fileExtension = ".ts";
+            const extension = ".mjs";
+            if (args.importer) {
+              const p = path.join(args.resolveDir, args.path);
+              let tsPath = `${p}${fileExtension}`;
+              let tsxPath = `${p}${fileExtension}x`;
+
+              let importPath = "";
+              if (fs.existsSync(tsPath) || fs.existsSync(tsxPath)) {
+                importPath = args.path + extension;
+              } else {
+                tsPath = path.join(
+                  args.resolveDir,
+                  args.path,
+                  `index${fileExtension}`
+                );
+                tsxPath = path.join(
+                  args.resolveDir,
+                  args.path,
+                  `index${fileExtension}x`
+                );
+                if (fs.existsSync(tsPath) || fs.existsSync(tsxPath)) {
+                  importPath = `${args.path}/index${extension}`;
+                }
+              }
+              return { path: importPath, external: true };
+            }
+          });
+        },
+      },
+    ],
+  },
+]);
