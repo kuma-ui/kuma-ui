770ef6f95573e11f7762ec50e26c19ae6e916b11
nojiritakeshi <takesoccer08nine@ezweb.ne.jp>
2023-07-22 17:30:53 +0900

    convert to sync method

--- packages/babel-plugin/src/__test__/css.test.ts
+++ packages/babel-plugin/src/__test__/css.test.ts
@@ -9,31 +9,31 @@ import path from "path";
 
 describe("css function", () => {
   describe("Snapshot tests", () => {
-    test("basic usage should match snapshot", async () => {
+    test("basic usage should match snapshot", () => {
       // Arrange
       const inputCode = `
         import { css } from '@kuma-ui/core'
         const style = css\`color: red;\`
       `;
       // Act
-      const result = await babelTransform(inputCode);
+      const result = babelTransform(inputCode);
       // Assert
       expect(getExpectSnapshot(result)).toMatchSnapshot();
     });
 
-    test("using space props should match snapshot", async () => {
+    test("using space props should match snapshot", () => {
       // Arrange
       const inputCode = `
         import { css } from '@kuma-ui/core'
         const style = css\`padding: 2px;\`
       `;
       // Act
-      const result = await babelTransform(inputCode);
+      const result = babelTransform(inputCode);
       // Assert
       expect(getExpectSnapshot(result)).toMatchSnapshot();
     });
 
-    test("using pseudo props should match snapshot", async () => {
+    test("using pseudo props should match snapshot", () => {
       // Arrange
       const inputCode = `
         import { css } from '@kuma-ui/core'
@@ -44,7 +44,7 @@ describe("css function", () => {
         \`
       `;
       // Act
-      const result = await babelTransform(inputCode);
+      const result = babelTransform(inputCode);
       // Assert
       expect(getExpectSnapshot(result)).toMatchSnapshot();
     });
--- packages/vite/src/index.ts
+++ packages/vite/src/index.ts
@@ -5,7 +5,6 @@ import { buildSync } from "esbuild";
 import _eval from "eval";
 import { theme, sheet } from "@kuma-ui/sheet";
 import { readdirSync } from "fs";
-import { StyleGenerator } from "@kuma-ui/system";
 
 export default function kumaUI(): Plugin {
   let mode: "build" | "serve";
@@ -53,13 +52,13 @@ export default function kumaUI(): Plugin {
       });
       return config;
     },
-    async transform(code: string, id: string) {
+    transform(code: string, id: string) {
       if (id.includes("@kuma-ui")) return;
       if (!/\.(t|j)(s|sx)?$/.test(id)) return;
       if (/node_modules/.test(id)) return;
       if (!code.includes("@kuma-ui/core")) return;
 
-      const result = await transform(code, id);
+      const result = transform(code, id);
       if (!result?.code) return;
       const cssFilename = path.normalize(`${id.replace(/\.[jt]sx?$/, "")}.css`);
       const cssRelativePath = path
@@ -86,7 +85,7 @@ export default function kumaUI(): Plugin {
         return id;
       }
     },
-    handleHotUpdate({ server, file }) {
+    handleHotUpdate({ server }) {
       sheet.reset();
       server.ws.send({ type: "full-reload" });
     },
