4fafbef71fc404eeac97492aaacb82f558f7247d
poteboy <poteboy.dev@gmail.com>
2023-07-23 12:44:46 +0900

    replace kuma component with raw html

--- packages/compiler/src/compile.ts
+++ packages/compiler/src/compile.ts
@@ -48,7 +48,7 @@ const compile = (
         extractedPropsMap
       );
       if (result) css.push(result.css);
-      optimize(componentName, openingElement);
+      optimize(componentName, openingElement, extractedPropsMap["as"]);
     }
   });
   return { code: source.getFullText(), id, css: css.join(" ") };
--- packages/compiler/src/optimizer/optimize.ts
+++ packages/compiler/src/optimizer/optimize.ts
@@ -1,13 +1,57 @@
-import { componentList } from "@kuma-ui/core/components/componentList";
-import { JsxOpeningElement, JsxSelfClosingElement } from "ts-morph";
+import {
+  componentList,
+  defaultComponentTag,
+} from "@kuma-ui/core/components/componentList";
+import {
+  JsxOpeningElement,
+  JsxSelfClosingElement,
+  Node,
+  SyntaxKind,
+} from "ts-morph";
+import { isStyledProp, isPseudoProps } from "@kuma-ui/system";
 
 export const optimize = (
   componentName: (typeof componentList)[keyof typeof componentList],
-  jsxElement: JsxOpeningElement | JsxSelfClosingElement
+  jsxElement: JsxOpeningElement | JsxSelfClosingElement,
+  as?: string
 ) => {
-  const isOptimizable = jsxElement.getAttributes().some((attr) => {
-    console.log("-------------------");
-    console.log(attr.getText());
+  const isOptimizable = jsxElement.getAttributes().every((attr) => {
+    if (hasDynamicProp(attr.getText())) return false;
+    if (Node.isJsxSpreadAttribute(attr)) return false;
     return true;
   });
+
+  if (!isOptimizable) return;
+
+  const rawHTMLTag = (() => {
+    const tag = defaultComponentTag[componentName];
+    if (as && typeof as === "string") return as;
+    else {
+      if (typeof tag === "string") return tag;
+      return "div";
+    }
+  })();
+
+  if (Node.isJsxOpeningElement(jsxElement)) {
+    const jsxElementParent = jsxElement.getParentIfKind(SyntaxKind.JsxElement);
+    if (jsxElementParent) {
+      const closingElement = jsxElementParent.getClosingElement();
+      jsxElement.getTagNameNode().replaceWithText(rawHTMLTag);
+      closingElement.getTagNameNode().replaceWithText(rawHTMLTag);
+    }
+  } else if (Node.isJsxSelfClosingElement(jsxElement)) {
+    jsxElement.getTagNameNode().replaceWithText(rawHTMLTag);
+  }
 };
+
+function hasDynamicProp(key: string) {
+  if (
+    isStyledProp(key) ||
+    isPseudoProps(key) ||
+    key === "variant" ||
+    key === "as"
+  ) {
+    return true;
+  }
+  return false;
+}
