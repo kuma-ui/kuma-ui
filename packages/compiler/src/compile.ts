ae181592eaa73785a222cec77482b9db47d164c4
kotarella1110 <kotarella1110@gmail.com>
2023-07-31 13:15:27 +0900

    refactor: move styled logic from babel-plugin to compiler

--- packages/compiler/src/compile.ts
+++ packages/compiler/src/compile.ts
@@ -52,15 +52,42 @@ const compile = (
       if (result) css.push(result.css);
     }
     if (Node.isTaggedTemplateExpression(node)) {
+      const tag = node.getTag();
       // css``
-      const cssTag = node.getTag();
-      if (cssTag.getText() !== bindings["css"]) return;
-      const cssTemplateLiteral = node.getTemplate();
-      if (Node.isNoSubstitutionTemplateLiteral(cssTemplateLiteral)) {
-        const cssString = cssTemplateLiteral.getLiteralText();
-        const className = cssString ? sheet.parseCSS(cssString) : undefined;
-        if (className) {
-          node.replaceWithText(JSON.stringify(className));
+      if (Node.isIdentifier(tag) && tag.getText() === bindings["css"]) {
+        const cssTemplateLiteral = node.getTemplate();
+        if (Node.isNoSubstitutionTemplateLiteral(cssTemplateLiteral)) {
+          const cssString = cssTemplateLiteral.getLiteralText();
+          const className = cssString ? sheet.parseCSS(cssString) : undefined;
+          if (className) {
+            node.replaceWithText(JSON.stringify(className));
+          }
+        }
+      }
+      // styled("xxx")``
+      if (
+        Node.isCallExpression(tag) &&
+        tag.getExpressionIfKind(SyntaxKind.Identifier)?.getText() ===
+          bindings["styled"]
+      ) {
+        const cssTemplateLiteral = node.getTemplate();
+        if (Node.isNoSubstitutionTemplateLiteral(cssTemplateLiteral)) {
+          const componentArg = tag.getArguments()[0];
+          const component = Node.isStringLiteral(componentArg)
+            ? componentArg.getLiteralText()
+            : "div";
+          const cssString = cssTemplateLiteral.getLiteralText();
+          const className = cssString ? sheet.parseCSS(cssString) : undefined;
+          if (className) {
+            node.replaceWithText(`props => {
+  const existingClassName = props.className || "";
+  const newClassName = "${className || ""}";
+  const combinedClassName = [existingClassName, newClassName].filter(Boolean).join(" ");
+  return <${
+    bindings["Box"]
+  } as="${component}" {...props} className={combinedClassName} IS_KUMA_DEFAULT />;
+}`);
+          }
         }
       }
     }
