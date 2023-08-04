6695f4e72dd8d6b7410ad84d06f4bf877dd7dd70
Yuku Kotani <yukukotani@gmail.com>
2023-08-04 16:17:05 +0900

    add background long-hand props

--- packages/system/src/background.test.ts
+++ packages/system/src/background.test.ts
@@ -4,18 +4,35 @@ import { describe, expect, test } from "vitest";
 describe("background utility function", () => {
   const testCases: Array<[BackgroundProps, string, string]> = [
     [
+      { backgroundImage: "url('img_tree.png')" },
+      "background-image: url('img_tree.png');",
+      "",
+    ],
+    [
       { bgImage: "url('img_tree.png')" },
       "background-image: url('img_tree.png');",
       "",
     ],
+    [{ backgroundSize: "cover" }, "background-size: cover;", ""],
     [{ bgSize: "cover" }, "background-size: cover;", ""],
     [{ bgSize: 10 }, "background-size: 10px;", ""],
+    [{ backgroundPosition: "center" }, "background-position: center;", ""],
     [{ bgPosition: "center" }, "background-position: center;", ""],
+    [{ backgroundPositionX: 10 }, "background-position-x: 10px;", ""],
     [{ bgPositionX: 10 }, "background-position-x: 10px;", ""],
+    [{ backgroundPositionY: "25%" }, "background-position-y: 25%;", ""],
     [{ bgPositionY: "25%" }, "background-position-y: 25%;", ""],
+    [{ backgroundRepeat: "no-repeat" }, "background-repeat: no-repeat;", ""],
     [{ bgRepeat: "no-repeat" }, "background-repeat: no-repeat;", ""],
+    [{ backgroundAttachment: "fixed" }, "background-attachment: fixed;", ""],
     [{ bgAttachment: "fixed" }, "background-attachment: fixed;", ""],
+    [{ backgroundClip: "border-box" }, "background-clip: border-box;", ""],
     [{ bgClip: "border-box" }, "background-clip: border-box;", ""],
+    [
+      { backgroundOrigin: "content-box" },
+      "background-origin: content-box;",
+      "",
+    ],
     [{ bgOrigin: "content-box" }, "background-origin: content-box;", ""],
   ];
 
