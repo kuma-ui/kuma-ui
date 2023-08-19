0027a429067311661165b7aed59029cdd9ca6383
Yuku Kotani <yukukotani@gmail.com>
2023-08-19 21:19:42 +0900

    fix test

--- packages/system/src/valueConverters.test.ts
+++ packages/system/src/valueConverters.test.ts
@@ -1,6 +1,6 @@
 import { theme } from "@kuma-ui/sheet";
 import { beforeAll, describe, expect, test } from "vitest";
-import { spaceConverter } from "./valueConverters";
+import { sizeConverter, spaceConverter } from "./valueConverters";
 
 describe("spaceConverter", () => {
   beforeAll(() => {
@@ -43,21 +43,21 @@ describe("sizeConverter", () => {
   });
 
   test("should correctly convert theme token value", () => {
-    const converted = spaceConverter("sm");
+    const converted = sizeConverter("sm");
 
     // Assert
     expect(converted).toBe("1rem");
   });
 
   test("should correctly convert raw string value", () => {
-    const converted = spaceConverter("1px");
+    const converted = sizeConverter("1px");
 
     // Assert
     expect(converted).toBe("1px");
   });
 
   test("should correctly convert raw number value", () => {
-    const converted = spaceConverter(1);
+    const converted = sizeConverter(1);
 
     // Assert
     expect(converted).toBe("1px");
