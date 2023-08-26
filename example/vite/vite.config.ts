cdca68ababa304b78bb907ec407c3818bf406811
taishinaritomi <taishinaritomi@gmail.com>
2023-08-26 23:31:18 +0900

    fix code format

--- example/vite/vite.config.ts
+++ example/vite/vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import kumaUI from "@kuma-ui/vite";
-import Inspect from 'vite-plugin-inspect'
+import Inspect from "vite-plugin-inspect";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), kumaUI(),     Inspect({
-    build: true,
-    outputDir: '.inspect'
-  })],
+  plugins: [
+    react(),
+    kumaUI(),
+    Inspect({
+      build: true,
+      outputDir: ".inspect",
+    }),
+  ],
 });
