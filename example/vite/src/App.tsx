18b06b4032c8b5b605d832a6f398da8294711239
poteboy <poteboy.dev@gmail.com>
2023-08-26 20:44:33 +0900

    wip

--- example/vite/src/App.tsx
+++ example/vite/src/App.tsx
@@ -1,19 +1,161 @@
-import { Box, HStack, styled, css, Select as S, Text } from "@kuma-ui/core";
-import { Dynamic } from "./Dynamic";
+// import { Box, HStack, styled, css, Select as S, Text } from "@kuma-ui/core";
+// import { Dynamic } from "./Dynamic";
 
-function App() {
-  const red = "red";
+// function App() {
+//   const red = "red";
+//   return (
+//     <HStack flexDir={["row", "column"]} gap="spacings.4">
+//       <Text variant={"primary"}>hello</Text>
+//       <Dynamic key={1} />
+//       <Dynamic key={2} />
+//     </HStack>
+//   );
+// }
+
+// const HelloWorld = styled("p")`
+//   color: red;
+// `;
+
+// export default App;
+
+import React from "react";
+import { Box, Heading, css, Text, Link } from "@kuma-ui/core";
+import { useState, useEffect } from "react";
+
+export default () => {
+  const isDark = useState(false);
+
+  useEffect(() => {
+    const html = document.getElementsByTagName("html")[0];
+    isDark[1](html.style.colorScheme === "dark");
+    const observer = new MutationObserver(function (mutations) {
+      mutations.forEach(function (mutation) {
+        // 'colorScheme' is not a standard style property, but we monitor it here
+        // because 'nextra-theme-docs' applies its own color scheme settings.
+        isDark[1](html.style.colorScheme === "dark");
+      });
+    });
+    observer.observe(html, { attributeFilter: ["style"] });
+    return () => observer.disconnect();
+  }, []);
+
   return (
-    <HStack flexDir={["row", "column"]} gap="spacings.4">
-      <Text variant={"primary"}>hello</Text>
-      <Dynamic key={1} />
-      <Dynamic key={2} />
-    </HStack>
+    <Box m={["40px auto", "80px auto 0px"]}>
+      <Box position="relative">
+        <Box role="presentation" display={["initial"]} bg="white">
+          <Box
+            bg="radial-gradient(circle at 75% 50%, hsl(186 75% 85%), rgba(255, 255, 255, 0) 25%), radial-gradient(circle at 60% 30%, hsl(200 75% 85%), rgba(255, 255, 255, 0) 10%)"
+            position="fixed"
+            inset={["0px", "100px"]}
+            zIndex="-1"
+          />
+        </Box>
+        <Heading
+          fontSize={["2.25rem", "3.25rem"]}
+          fontWeight={800}
+          className={css`
+            letter-spacing: -0.025em;
+            line-height: 1;
+            @media (max-width: sm) {
+              line-height: 2.5rem;
+            }
+          `}
+        >
+          Empower Your Web with Ultimate Performance and Flexibility
+        </Heading>
+        <Text
+          color="#718096"
+          mt="1.5rem"
+          fontSize="1.125rem"
+          className={css`
+            line-height: 1.75rem;
+            @media (prefers-color-scheme: dark) {
+              mix-blend-mode: plus-lighter;
+            }
+          `}
+          style={{
+            mixBlendMode: isDark[0] ? "plus-lighter" : "initial",
+          }}
+        >
+          With Kuma UI's headless, zero-runtime UI components, build
+          top-performing websites effortlessly, while enjoying the best
+          developer experience. Say hello to the future of CSS-in-JS
+        </Text>
+        <Box
+          p={40}
+          display="flex"
+          flexDir={["column", "row"]}
+          mx="auto"
+          justify="center"
+          gap="1rem"
+        >
+          <Link
+            href="/docs"
+            bg="#292e32"
+            color="white"
+            px="1.5rem"
+            lineHeight="45px"
+            borderRadius="6px"
+            height="3rem"
+            textAlign="center"
+            cursor="pointer"
+            fontWeight={600}
+            _hover={{
+              opacity: 0.9,
+            }}
+          >
+            Get Started
+          </Link>
+        </Box>
+      </Box>
+      <Box as="section" mt="5rem">
+        <Box
+          display="grid"
+          gap="1.5rem"
+          gridTemplateColumns={[
+            "repeat(1,minmax(0,1fr))",
+            "repeat(1,minmax(0,1fr))",
+            "repeat(3,minmax(0,1fr))",
+          ]}
+        ></Box>
+      </Box>
+    </Box>
   );
-}
+};
 
-const HelloWorld = styled("p")`
-  color: red;
-`;
-
-export default App;
+const gridData = [
+  {
+    emoji: "⚡️",
+    title: "Zero-runtime",
+    description:
+      "Deliver blazing-fast websites with zero-runtime CSS extraction.",
+  },
+  {
+    emoji: "🔮",
+    title: "Autocomplete",
+    description: "Experience seamless development with style autocompletion.",
+  },
+  {
+    emoji: "🛠️",
+    title: "Headless Components",
+    description:
+      "Enjoy maximum customization freedom with our headless components.",
+  },
+  {
+    emoji: "🤝",
+    title: "Hybrid Approach",
+    description:
+      "The best of both worlds—support for any writing style with our hybrid approach.",
+  },
+  {
+    emoji: "🚀",
+    title: "RSC Support",
+    description:
+      "Stay updated with cutting-edge Next.js technology via our RSC support.",
+  },
+  {
+    emoji: "🥇",
+    title: "Familiar DX",
+    description: "Delight in the ultimate DX with our familiar API design.",
+  },
+];
--- packages/compiler/src/optimizer/optimize.ts
+++ packages/compiler/src/optimizer/optimize.ts
@@ -39,19 +39,26 @@ export const optimize = (
 
   jsxElement.getAttribute("IS_KUMA_DEFAULT")?.remove();
 
+  // console.log(jsxElement.getSourceFile().getFullText());
+
   if (Node.isJsxOpeningElement(jsxElement)) {
     const jsxElementParent = jsxElement.getParentIfKind(SyntaxKind.JsxElement);
-    if (jsxElementParent) {
-      const closingElement = jsxElementParent.getClosingElement();
-      jsxElement
-        .getFirstDescendantByKind(SyntaxKind.Identifier)
-        ?.replaceWithText(rawHTMLTag);
+    const children = jsxElement.getChildren();
+    console.log(jsxElementParent?.getText());
 
-      closingElement
-        .getFirstDescendantByKind(SyntaxKind.Identifier)
-        ?.replaceWithText(rawHTMLTag);
+    if (jsxElementParent) {
+      const children = jsxElement.getChildren();
+
+      const closingElement = jsxElementParent.getClosingElement();
+      const openingElement = jsxElementParent.getOpeningElement();
+
+      openingElement.getTagNameNode().replaceWithText(rawHTMLTag);
+      closingElement.getTagNameNode().replaceWithText(rawHTMLTag);
+
+      console.log(openingElement.getText());
     }
   } else if (Node.isJsxSelfClosingElement(jsxElement)) {
+    jsxElement.getTagNameNode().replaceWithText(rawHTMLTag);
     jsxElement
       .getFirstDescendantByKind(SyntaxKind.Identifier)
       ?.replaceWithText(rawHTMLTag);
@@ -68,4 +75,64 @@ function hasDynamicProp(key: string) {
     return true;
   }
   return false;
+}
+
+function processElement(
+  jsxElement: JsxOpeningElement | JsxSelfClosingElement,
+  componentName: (typeof componentList)[keyof typeof componentList],
+  as?: string
+) {
+  // まず子要素を処理します
+  const children = jsxElement.getChildren();
+  children.forEach((child) => {
+    if (
+      Node.isJsxOpeningElement(child) ||
+      Node.isJsxSelfClosingElement(child)
+    ) {
+      processElement(child, componentName, as);
+    }
+  });
+
+  const isOptimizable = jsxElement.getAttributes().every((attrLike) => {
+    if (Node.isJsxSpreadAttribute(attrLike)) return false;
+    const attr = attrLike.asKindOrThrow(SyntaxKind.JsxAttribute);
+    if (hasDynamicProp(attr.getNameNode().getText())) return false;
+    return true;
+  });
+
+  if (!isOptimizable) return;
+
+  const rawHTMLTag = getRawHTMLTag(componentName, as, jsxElement);
+
+  if (Node.isJsxOpeningElement(jsxElement)) {
+    const jsxElementParent = jsxElement.getParentIfKind(SyntaxKind.JsxElement);
+
+    if (jsxElementParent) {
+      const closingElement = jsxElementParent.getClosingElement();
+      const openingElement = jsxElementParent.getOpeningElement();
+
+      openingElement.getTagNameNode().replaceWithText(rawHTMLTag);
+      closingElement.getTagNameNode().replaceWithText(rawHTMLTag);
+    }
+  } else if (Node.isJsxSelfClosingElement(jsxElement)) {
+    jsxElement.getTagNameNode().replaceWithText(rawHTMLTag);
+    jsxElement
+      .getFirstDescendantByKind(SyntaxKind.Identifier)
+      ?.replaceWithText(rawHTMLTag);
+  }
+}
+
+function getRawHTMLTag(
+  componentName: (typeof componentList)[keyof typeof componentList],
+  as?: string,
+  jsxElement?: JsxOpeningElement | JsxSelfClosingElement
+): string {
+  const tag = defaultComponentTag[componentName];
+  if (!!as && typeof as === "string") {
+    jsxElement?.getAttribute("as")?.remove();
+    return (as as string).replace(/['"`]/g, "");
+  } else {
+    if (typeof tag === "string") return tag;
+    return "div";
+  }
 }
