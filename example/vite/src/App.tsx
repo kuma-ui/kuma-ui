1be1522faebf41ab51eadd9d8b101b73ba829d7a
poteboy <poteboy.dev@gmail.com>
2023-08-30 21:28:19 +0900

    zero bundle when application is composed only of statiic styles

--- example/vite/src/App.tsx
+++ example/vite/src/App.tsx
@@ -6,14 +6,8 @@ function App() {
   return (
     <HStack flexDir={["row", "column"]} gap="spacings.4">
       <Text>hello</Text>
-      <Dynamic key={1} />
-      <Dynamic key={2} />
     </HStack>
   );
 }
-
-const HelloWorld = styled("p")`
-  color: red;
-`;
 
 export default App;
