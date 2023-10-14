668d02491e478d2fab8a56d8ef6a3761379652f0
poteboy <poteboy.dev@gmail.com>
2023-10-15 01:47:13 +0900

    chore: modify JSDoc for `ensureReactImport`

--- packages/babel-plugin/src/ensureReactImport.ts
+++ packages/babel-plugin/src/ensureReactImport.ts
@@ -2,11 +2,13 @@ import { NodePath } from "@babel/core";
 import { types } from "@babel/core";
 
 /**
- * Ensures that a React import declaration exists in the given program.
- * If not, it adds the import declaration at the beginning of the program.
+ * Ensures that React is imported in the given program to satisfy JSX's Classic runtime requirement.
+ * This function adds a React import declaration at the beginning of the program with a unique alias to avoid potential naming conflicts.
  *
- * @param {NodePath<t.Program>} programPath - The path to the Program node in the AST.
- * @param {typeof t} t - The Babel types.
+ * Note: The unique alias "__React_Kuma_" is utilized in Kuma's compiler to safely refer to React (e.g., when using React.forwardRef to wrap styled components) without interfering with user-defined variable names.
+ *
+ * @param {NodePath<types.Program>} programPath - The path to the Program node in the AST.
+ * @param {typeof types} t - The Babel types.
  */
 export function ensureReactImport(
   programPath: NodePath<types.Program>,
