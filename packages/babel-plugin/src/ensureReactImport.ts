25dd0b41c8856504761234638709bcf99db5c6a1
poteboy <59927325+poteboy@users.noreply.github.com>
2023-10-21 11:30:50 +0900

    Enable `forwardRef` Usage in Components Created through the `styled` API (#357)

--- packages/babel-plugin/src/ensureReactImport.ts
+++ packages/babel-plugin/src/ensureReactImport.ts
@@ -2,29 +2,21 @@ import { NodePath } from "@babel/core";
 import { types } from "@babel/core";
 
 /**
- * Ensures that a React import declaration exists in the given program.
- * If not, it adds the import declaration at the beginning of the program.
+ * Ensures React is imported in the given program to satisfy JSX's Classic runtime requirement.
+ * While React might already be imported, we can't determine the specifier name if a default import exists.
+ * Thus, this function adds a separate React import declaration at the beginning of the program with a unique alias "__KUMA_REACT__".
+ * This unique alias allows Kuma's compiler to safely refer to React (e.g., when using React.forwardRef to wrap styled components) without interfering with user-defined variable names.
  *
- * @param {NodePath<t.Program>} programPath - The path to the Program node in the AST.
- * @param {typeof t} t - The Babel types.
+ * @param {NodePath<types.Program>} programPath - The path to the Program node in the AST.
+ * @param {typeof types} t - The Babel types.
  */
 export function ensureReactImport(
   programPath: NodePath<types.Program>,
   t: typeof types,
 ) {
-  let hasReactImport = false;
-
-  programPath.node.body.forEach((node) => {
-    if (t.isImportDeclaration(node) && node.source.value === "react") {
-      hasReactImport = true;
-    }
-  });
-
-  if (!hasReactImport) {
-    const reactImportDeclaration = t.importDeclaration(
-      [t.importDefaultSpecifier(t.identifier("React"))],
-      t.stringLiteral("react"),
-    );
-    programPath.unshiftContainer("body", reactImportDeclaration);
-  }
+  const reactImportDeclaration = t.importDeclaration(
+    [t.importDefaultSpecifier(t.identifier("__KUMA_REACT__"))],
+    t.stringLiteral("react"),
+  );
+  programPath.unshiftContainer("body", reactImportDeclaration);
 }
