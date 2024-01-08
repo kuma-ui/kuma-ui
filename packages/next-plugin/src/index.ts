1463b669b095d9144b8a9598b5d1f631ba6cc75e
taishinaritomi <taishinaritomi@gmail.com>
2024-01-08 19:44:01 +0900

    fix next-plugin

--- packages/next-plugin/src/index.ts
+++ packages/next-plugin/src/index.ts
@@ -4,7 +4,6 @@ import { type Configuration, type RuleSetRule } from "webpack";
 import KumaUIWebpackPlugin from "@kuma-ui/webpack-plugin";
 import { lazyPostCSS } from "next/dist/build/webpack/config/blocks/css/index.js";
 import { getGlobalCssLoader } from "next/dist/build/webpack/config/blocks/css/loaders/index.js";
-import { findPagesDir } from "next/dist/lib/find-pages-dir.js";
 import type { ConfigurationContext } from "next/dist/build/webpack/config/utils.js";
 
 const getSupportedBrowsers = (dir: string, isDevelopment: boolean) => {
@@ -21,65 +20,39 @@ const getSupportedBrowsers = (dir: string, isDevelopment: boolean) => {
 const kumaUiConfig = (nextConfig: NextConfig): NextConfig => {
   return {
     webpack(config: Configuration & ConfigurationContext, options) {
-      // const { dir, dev, isServer } = options;
-      // const { appDir } = findPagesDir(
-      //   dir,
-      //   !!options.config.experimental.appDir,
-      // );
+      const { dir, dev, isServer } = options;
 
-      // const cssRules = (
-      //   config.module?.rules?.find(
-      //     (rule) =>
-      //       typeof rule === "object" &&
-      //       Array.isArray(rule.oneOf) &&
-      //       rule.oneOf.some(
-      //         ({ test }) =>
-      //           test instanceof RegExp &&
-      //           typeof test.test === "function" &&
-      //           test.test("filename.css"),
-      //       ),
-      //   ) as RuleSetRule
-      // )?.oneOf;
+      const cssRules = (
+        config.module?.rules?.find(
+          (rule) =>
+            typeof rule === "object" &&
+            Array.isArray(rule.oneOf) &&
+            rule.oneOf.some(
+              ({ test }) =>
+                test instanceof RegExp &&
+                typeof test.test === "function" &&
+                test.test("filename.css"),
+            ),
+        ) as RuleSetRule
+      )?.oneOf;
 
-      // const appDirOptions = appDir
-      //   ? {
-      //       hasAppDir: true,
-      //       experimental: { appDir: true },
-      //     }
-      //   : {};
-
-      // cssRules?.push({
-      //   test: /.css$/i,
-      //   sideEffects: true,
-      //   use: getGlobalCssLoader(
-      //     {
-      //       assetPrefix: config.assetPrefix,
-      //       isClient: !isServer,
-      //       isServer,
-      //       isDevelopment: dev,
-      //       experimental: {},
-      //       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- FIXME
-      //       future: nextConfig.future || {},
-      //       ...appDirOptions,
-      //     } as ConfigurationContext,
-      //     () => lazyPostCSS(dir, getSupportedBrowsers(dir, dev), undefined),
-      //     [],
-      //   ),
-      // });
-
-      // config.module?.rules?.push({
-      //   test: /\.(tsx|ts|js|mjs|jsx)$/,
-      //   exclude: /node_modules/,
-      //   use: [
-      //     {
-      //       loader: KumaUIWebpackPlugin.loader,
-      //       options: {
-      //         virtualLoader: !appDir,
-      //         cssOutputDir: "./.next/cache/kuma",
-      //       },
-      //     },
-      //   ],
-      // });
+      cssRules?.push({
+        test: /.css$/i,
+        sideEffects: true,
+        use: getGlobalCssLoader(
+          {
+            assetPrefix: config.assetPrefix,
+            isClient: !isServer,
+            isServer,
+            isDevelopment: dev,
+            experimental: {},
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- FIXME
+            future: nextConfig.future || {},
+          } as ConfigurationContext,
+          () => lazyPostCSS(dir, getSupportedBrowsers(dir, dev), undefined),
+          [],
+        ),
+      });
 
       config.plugins?.push(new KumaUIWebpackPlugin());
       if (typeof nextConfig.webpack === "function") {
