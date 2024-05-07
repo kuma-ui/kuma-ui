857ce5a58c1bdfbab12e3912bebaaf1ebcabbe6d
poteboy <poteboy@poteboy.local>
2024-05-08 03:05:01 +0900

    export HOME env

--- website/setup-rust.sh
+++ website/setup-rust.sh
@@ -1,6 +1,7 @@
+export HOME=/vercel
 echo "Installing Rustup..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 echo "Installing wasm-pack..."
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
-pnpm build
+turbo build
