62f9784833a1387394ac9b6cf7a3638535853f8a
Thomas Lindstrøm <t@hom.as>
2024-07-15 15:17:46 +0200

    Add `cx` function to merge class names

--- packages/core/src/css.ts
+++ packages/core/src/css.ts
@@ -4,3 +4,7 @@ import { ThemeSystem } from "./theme";
 export const css = (_strings: TemplateStringsArray): string => {
   throw Error('Using the "css" in runtime is not supported.');
 };
+
+export const cx = (...classNames: (string | false | undefined)[]) => (
+	classNames.filter((className) => Boolean(className)).join(' ')
+);
