fdba9cc7c760a6652d2bb345867adfb5288946a3
poteboy <59927325+poteboy@users.noreply.github.com>
2024-09-03 16:10:34 +0900

    Add Content & ContentVisibility Props To System (#421)

--- example/vite/src/App.tsx
+++ example/vite/src/App.tsx
@@ -6,6 +6,7 @@ import {
   Select as S,
   Text,
   Button,
+  Link,
 } from "@kuma-ui/core";
 import { Dynamic } from "./Dynamic";
 import { MouseEventHandler, useEffect, useRef } from "react";
@@ -32,6 +33,16 @@ function App() {
       <Styled>Styled</Styled>
       <Text contain="size">日本語</Text>
       <Dynamic />
+      <Link
+        href="https://www.kuma-ui.com/"
+        target="_blank"
+        _after={{
+          content: '"🚀"',
+          fontSize: 24,
+        }}
+      >
+        Go to Our Website
+      </Link>
     </Box>
   );
 }
